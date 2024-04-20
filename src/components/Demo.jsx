8fba7b85ab15d7c7b441e9409226f288f29b4759
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-04-20 05:25:31 +0500

    Demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
+import { useEffect, useState } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const handleSubmit = async (e) => {
+    alert("Clic");
+  };
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700"
+          >
+            <p className="flex items-center">↵</p>
+          </button>
+        </form>
+      </div>
+      <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+        <ul></ul>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
