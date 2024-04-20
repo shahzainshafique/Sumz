bd1257d1760bb039e0b976f799c443f915bc0248
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-04-20 05:13:21 +0500

    Initial Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import { logo } from "../assets";
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          className="black_btn"
+          onClick={() => {
+            window.open("https://github.com/sumz-ai/sumz-ai");
+          }}
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />{" "}
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">Summarize your reading with a click of a button</h2>
+    </header>
+  );
+};
+
+export default Hero;
